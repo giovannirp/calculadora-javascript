08991b3f0f838b1a1aea6ec14e18a18ba4b9fb7d
Giovanni Ribeiro Pinto <giovanni.ribeiro@grupoltm.com.br>
2020-07-18 02:03:18 -0300

    acertando layout new

--- js/CalcController.js
+++ js/CalcController.js
@@ -3,17 +3,22 @@ class CalcController {
     constructor() {
 
         this._operation = [];
+        this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
+        this._timeEl = document.querySelector("#hora");
+        this._dataEl = document.querySelector("#data");
+
+        this._currentDate;
         this.initialize();
         this.initButtonsEvents();
     }
 
     initialize() {
-
+        this.setDisplayDateTime();
     }
 
     addEventListenerAll(element, events, fn) {
-        
+
         events.split(' ').forEach(event => {
 
             element.addEventListener(event, fn, false);
@@ -31,9 +36,46 @@ class CalcController {
 
     }
 
+    getLastOperation() {
+
+        return this._operation[this._operation.length - 1];
+
+    }
+
+    setLastOperation(value) {
+       return this._operation[this._operation.length -1 ] = value;
+
+    }
+
+    isOperator(value) {
+       // return (['+', '-', 'X', '%', '/'].indexOf(value) > - 1);
+        return (['+', '-', 'X', '%', '/'].indexOf(value) > -1);
+    }
+
     addOperation(value) {
-        
-        this._operation.push(value);
+
+        console.log('A', isNaN(this.getLastOperation()))
+
+        if(isNaN(this.getLastOperation())) {
+            //string
+
+            if(this.isOperator(value)) {
+                //trocar operador
+                this.setLastOperation(value);
+
+            } else if(isNaN(value)) {
+                   //outra coisa
+                   console.log(value);
+            } else {
+                this._operation.push(value);
+
+            }
+
+        } else {
+            //Number
+            let newValue = this.getLastOperation().toString() + value.toString();
+            this.setLastOperation(parseInt(newValue));
+        }
 
         console.log(this._operation);
 
@@ -54,16 +96,27 @@ class CalcController {
                 this.clearEntry();
                 break;
             case '÷':
+                this.addOperation('÷');
                 break;
             case 'X':
+                this.addOperation('X');
                 break;
             case '-':
+                this.addOperation('-');
                 break;
             case '+':
+                this.addOperation('+');
                 break;
             case '=':
+                this.addOperation('=');
                 break;
-           
+            case ',':
+                this.addOperation(',');
+                break;
+            case '%':
+                this.addOperation('%');
+                break;
+
             case '0':
             case '1':
             case '2':
@@ -75,11 +128,11 @@ class CalcController {
             case '8':
             case '9':
                 this.addOperation(parseInt(value));
-                 break;
+                break;
 
             default:
-            this.setError();
-            break;
+                this.setError();
+                break;
         }
 
     }
@@ -87,7 +140,7 @@ class CalcController {
     initButtonsEvents() {
         let buttons = document.querySelectorAll("#buttons .row button");
 
-        buttons.forEach((btn, index)=>{
+        buttons.forEach((btn, index) => {
 
             this.addEventListenerAll(btn, 'click drag', e => {
 
@@ -100,13 +153,38 @@ class CalcController {
             });
 
             this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
-                
+
                 btn.style.cursor = "cell";
 
             });
-            
+
         })
 
+    }
+    
+    setDisplayDateTime() {
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+            day: "2-digit",
+            month: "long",
+            year: "numeric"
+        });
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
+
+    get displayTime() {
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(value) {
+        return this._timeEl.innerHTML = value;
+    }
+
+    get displayDate() {
+        return this._dataEl.innerHTML;
+    }
+
+    set displayDate(value) {
+        return this._dataEl.innerHTML = value;
     }
 
     get displayCalc() {
@@ -115,6 +193,14 @@ class CalcController {
 
     set displayCalc(value) {
         this._displayCalcEl.innerHTML = value;
+    }
+
+    get currentDate() {
+        return new Date();
+    }
+
+    set currentDate(value) {
+        this._currentDate = value;
     }
 
 }
